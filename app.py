a413e36f1df1b0b8f019b62966c7538512167b6e
szeni23 <81260864+szeni23@users.noreply.github.com>
2023-08-21 17:31:28 +1200

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,37 @@ from page1 import page1
 from page2 import page2
 from page3 import page3
 
+# Set page config at the top of the script
+st.set_page_config(
+    page_title="ExpenseSplitter",
+    layout="wide"
+)
+
 tabs = {
     "Expense Splitter": page1,
-    "McCrocsMagicX AG edition": page2,
+    "McCrocsMagicX AG Splitter": page2,
     "About": page3,
 }
-def main():
 
+def main():
     if "update_sidebar" not in st.session_state:
         st.session_state.update_sidebar = False
 
-    if 'persons' not in st.session_state:
-        st.session_state.persons = []
+    # Initialize separate session state variables for each page's persons list.
+    if 'persons_page1' not in st.session_state:
+        st.session_state.persons_page1 = []
+
+    if 'persons_page2' not in st.session_state:
+        predefined_persons = ["CEO", "Lieferant", "Technigger", "Verplaner", "Pirat", "Koch", "Kanadier", "Milchma", "Chef Geheimdienst"]
+        st.session_state.persons_page2 = predefined_persons
+
+    # Determine the current page selection
+    selection = st.sidebar.radio(" ", list(tabs.keys()))
+
+    if selection == "McCrocsMagicX AG Splitter":
+        st.session_state.persons = st.session_state.persons_page2[:]
+    else:
+        st.session_state.persons = st.session_state.persons_page1[:]
 
     if 'expenses' not in st.session_state:
         st.session_state.expenses = []
@@ -27,11 +46,6 @@ def main():
     if 'previous_split_between' not in st.session_state:
         st.session_state.previous_split_between = []
 
-    st.set_page_config(
-        page_title="ExpenseSplitter",
-        layout="wide"
-    )
-    selection = st.sidebar.radio(" ", list(tabs.keys()))
     page = tabs[selection]
     with st.spinner(f"Load {selection}..."):
         page()
