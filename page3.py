91d1abc83767c9ec32d83cdc8c054c1abff2ff1d
szeni23 <81260864+szeni23@users.noreply.github.com>
2023-08-21 13:17:29 +1200

    Update page3.py

--- page3.py
+++ page3.py
@@ -1,29 +1,10 @@
 import streamlit as st
-paw = "1234"
+
 
 def page3():
-    st.title("Admin Area")
-
-    # Session state to check if user is authenticated
-    if 'authenticated' not in st.session_state:
-        st.session_state.authenticated = False
-
-    password_placeholder = st.empty()
-
-    if not st.session_state.authenticated:
-        password = password_placeholder.text_input("Enter Password", type='password')
-
-        if password == DEFINED_PASSWORD:
-            st.session_state.authenticated = True
-            password_placeholder.empty()
-            show_admin_tools()
-        elif password:
-            st.error("Incorrect password. Please try again.")
-    else:
-        show_admin_tools()
-
-
-def show_admin_tools():
-    st.write("Welcome to the admin area!")
-    st.write("Here, you can manage and configure your app settings and tools.")
-
+    st.write("""
+        This app was created by Rico.
+        
+        
+        The Expense Splitter is a project I built to simplify and visualize expense settlements among friends or groups. I hope you find it useful!
+        """)
